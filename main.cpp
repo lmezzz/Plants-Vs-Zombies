e48bd4a5df660ec00478398c1872f4fd09405ee2
lmezzz~ <ayaanra261@gmail.com>
2024-04-24 19:32:14 +0500

    structured the classes

--- main.cpp
+++ main.cpp
@@ -1,134 +1,110 @@
 #include <SFML/Graphics.hpp>
-#include <vector>
 #include <iostream>
+#include <vector>
 
-using namespace sf;
 using namespace std;
+using namespace sf;
 
 const int winWidth = 1280;
 const int winHeight = 720;
 const int ROWS = 10;
 const int COLUMNS = 10;
-const int blokWidth = winWidth / ROWS;
-const int blokHeight = winHeight / COLUMNS;
+const int blockWidth = winWidth / ROWS;
+const int blockHeight = winHeight / COLUMNS;
 const int zomRow = 3;
-const int houseColumn = 0;
 
-class Plant;
-class Zombie;
-
-struct Grid{
-    RectangleShape Cell;
-    bool IsOccupied;
-    Plant *plant;
-    Zombie *zombie;
-};
-
-class Plant {
-private:
-    Texture texture;
-    Texture shooterTexture;
+class Character {
+protected:
     Sprite sprite;
-    Sprite shooterSprite;
-    bool isAvailable;
-    bool hasShot; 
-    float scaleFactor;
-    float shooterScaleFactor;
-    int gridX;
-    int gridY;
-    int score;
+    Texture texture;
+
 public:
-    Plant(const Texture& texture, const Texture& shooterTexture) : texture(texture), shooterTexture(shooterTexture), sprite(), shooterSprite(), isAvailable(true), hasShot(false), scaleFactor(1.0f), shooterScaleFactor(1.0f), score(0) {
+    Character(const Texture& texture) : sprite() {
+        this->texture = texture;
         sprite.setTexture(texture);
-        shooterSprite.setTexture(shooterTexture);
-    }
-    void setPosition(int x, int y) {
-        sprite.setPosition(x * blokWidth, y * blokHeight);
-        gridX = x;
-        gridY = y;
-        isAvailable = false;
-
-        float scaleFactorX = static_cast<float>(blokWidth) / texture.getSize().x;
-        float scaleFactorY = static_cast<float>(blokHeight) / texture.getSize().y;
-        if (scaleFactorX < scaleFactorY) {
-            scaleFactor = scaleFactorX;
-        }
-        else {
-            scaleFactor = scaleFactorY;
-        }
-        sprite.setScale(scaleFactor, scaleFactor);
-        shooterSprite.setTexture(shooterTexture);
-        shooterSprite.setPosition(sprite.getPosition());
+        float scaleFactorX = static_cast<float>(blockWidth) / texture.getSize().x;
+        float scaleFactorY = static_cast<float>(blockHeight) / texture.getSize().y;
+        sprite.setScale(scaleFactorX, scaleFactorY);
     }
 
-    void draw(RenderWindow& window) const {
-        if (!isAvailable) {
-            window.draw(sprite);
-            if (hasShot)
-                window.draw(shooterSprite);
-        }
+    virtual void draw(RenderWindow& window) const {
+        window.draw(sprite);
     }
 
-    bool available() const {
-        return isAvailable;
-    }
-    void setAvailable(bool value) {
-        isAvailable = value;
-    }
-    int getGridX() const {
-        return gridX;
-    }
-    int getGridY() const {
-        return gridY;
-    }
-    int getScore() const {
-        return score;
-    }
-    void increaseScore(int value) {
-        score += value;
-    }
-    bool hasShotZombie() const {
-        return hasShot;
-    }
-    void shoot() {
-        hasShot = true;
-    }
-    void resetShot() {
-        hasShot = false;
-    }
-    FloatRect getShooterBounds() const {
-        Vector2f position = shooterSprite.getPosition();
-        Vector2f scale = shooterSprite.getScale();
-        Vector2u textureSize = shooterSprite.getTexture()->getSize();
-        float sizeX = static_cast<float>(textureSize.x) * scale.x;
-        float sizeY = static_cast<float>(textureSize.y) * scale.y;
-        return FloatRect(position.x, position.y, sizeX, sizeY);
+    FloatRect getBounds() const {
+        return sprite.getGlobalBounds();
     }
 };
 
-class Zombie {
-private:
-    Texture texture;
-    Sprite sprite;
+struct Grid {
+    bool isOccupied = true;
+    RectangleShape cell;
+    Character* character = nullptr;
+};
+
+class Bullet : public Character {
+public:
+    Bullet(const Texture& texture, const Vector2f& position) : Character(texture) {
+        sprite.setPosition(position);
+    }
+    void update(float deltaTime) {
+        sprite.move(50 * deltaTime, 0); // Adjust the speed here
+    }
+};
+
+class Plant : public Character {
+protected:
+    Grid cell;
+    vector<Bullet> bullets;
+    Texture bulletTexture;
+
+public:
+    Plant(const Texture& texture, const Texture& bulletTexture) : Character(texture), bulletTexture(bulletTexture), cell() {}
+
+    void setPosition(int x, int y) {
+        sprite.setPosition(x * blockWidth, y * blockHeight);
+        cell.isOccupied = false;
+        cell.character = this;
+    }
+
+    void draw(RenderWindow& window) const override {
+        window.draw(sprite);
+        for (size_t i = 0; i < bullets.size(); i++) {
+            bullets[i].draw(window);
+        }
+    }
+
+    void shoot(const Vector2f& targetPosition) {
+        Bullet bullet(bulletTexture, Vector2f(sprite.getPosition().x + sprite.getGlobalBounds().width / 2, sprite.getPosition().y));
+        bullets.push_back(bullet);
+    }
+
+    void updateBullets(float deltaTime) {
+        for (size_t i = 0; i < bullets.size(); i++) {
+            bullets[i].update(deltaTime);
+        }
+    }
+
+    void drawBullets(RenderWindow& window) const {
+        for (size_t i = 0; i < bullets.size(); i++) {
+            bullets[i].draw(window);
+        }
+    }
+};
+
+class Zombie : public Character {
+protected:
     int health;
     float speed;
+
 public:
-
-    Zombie(const Texture& texture, int health, float speed) : texture(texture), sprite(), health(health), speed(speed) {
-        sprite.setTexture(texture);
-        float scaleFactorX = static_cast<float>(blokWidth) / texture.getSize().x;
-        float scaleFactorY = static_cast<float>(blokHeight) / texture.getSize().y;    // Scale down to fit in a 120x72 box
-        sprite.setScale(scaleFactorX, scaleFactorY);
-        sprite.setPosition(blokWidth * 9, blokHeight * (zomRow));   //place it on the right side
+    Zombie(const Texture& texture, int health, float speed) : Character(texture), health(health), speed(speed) {
+        sprite.setPosition(blockWidth * 9, blockHeight * zomRow);
     }
-    
+
     void update(float deltaTime) {
-        float movement = speed * deltaTime ;
+        float movement = speed * deltaTime;
         sprite.move(-movement, 0); // Move towards the left
-    }
-
-    void draw(RenderWindow& window) const {
-        window.draw(sprite);
     }
 
     void takeDamage(int damage) {
@@ -137,46 +113,6 @@ public:
 
     bool isDead() const {
         return health <= 0;
-    }
-
-    FloatRect getBounds() const {
-        Vector2f position = sprite.getPosition();
-        Vector2f scale = sprite.getScale();
-        Vector2u textureSize = sprite.getTexture()->getSize();
-        float sizeX = static_cast<float>(textureSize.x) * scale.x;
-        float sizeY = static_cast<float>(textureSize.y) * scale.y;
-        return FloatRect(position.x, position.y, sizeX, sizeY);
-    }
-};
-
-class ShooterBall {
-private:
-    Texture texture;
-    Sprite sprite;
-public:
-    ShooterBall(const Texture& texture, const Vector2f& position) : texture(texture), sprite() {
-        sprite.setTexture(texture);
-        float scaleFactorX = static_cast<float>(blokWidth) / texture.getSize().x;
-        float scaleFactorY = static_cast<float>(blokHeight) / texture.getSize().y;    // Scale down to fit in a 120x72 box
-        sprite.setScale(scaleFactorX, scaleFactorY);
-        sprite.setPosition(position);
-    }
-
-    void update(float deltaTime) {
-        sprite.move(100 * deltaTime, 0); // Adjust the velocity here
-    }
-
-    void draw(RenderWindow& window) const {
-        window.draw(sprite);
-    }
-
-    FloatRect getBounds() const {
-        Vector2f position = sprite.getPosition();
-        Vector2f scale = sprite.getScale();
-        Vector2u textureSize = sprite.getTexture()->getSize();
-        float sizeX = static_cast<float>(textureSize.x) * scale.x;
-        float sizeY = static_cast<float>(textureSize.y) * scale.y;
-        return FloatRect(position.x, position.y, sizeX, sizeY);
     }
 };
 
@@ -198,55 +134,51 @@ int main() {
         return 1;
     }
 
-    Texture shooterTexture;
-    if (!shooterTexture.loadFromFile("Images\\bullet.png")) {
+    Texture bulletTexture;
+    if (!bulletTexture.loadFromFile("Images\\bullet.png")) {
         return 1;
     }
 
     vector<Plant> AllPlants;//changed 
     Vector2f CellSize;
-    CellSize.x = blokWidth;
-    CellSize.y = blokHeight;
+    CellSize.x = blockWidth;
+    CellSize.y = blockHeight;
     Grid Cells[10][10];
     RectangleShape SampleCell;
     SampleCell.setSize(CellSize);
 
     Zombie zombie(zombieTexture, 5, 80.0f); // Increased zombie speed
 
-    vector<ShooterBall> shooterBalls;
+    vector<Bullet> bullets;
 
     Clock clock;
 
     while (window.isOpen()) {
         float timeTaken = clock.restart().asSeconds();
-
         Event event;
-        
-
         window.clear();
-
         for (int y = 0; y < ROWS; y++) {
             for (int x = 0; x < COLUMNS; x++) {
-                Cells[y][x].Cell = SampleCell;
-                Cells[y][x].Cell.setPosition(x * blokWidth, y * blokHeight);
+                Cells[y][x].cell = SampleCell;
+                Cells[y][x].cell.setPosition(x * blockWidth, y * blockHeight);
                 if (x == 0) {
-                    Cells[y][x].Cell.setFillColor(brown);
+                    Cells[y][x].cell.setFillColor(brown);
                 }
                 else if (x == COLUMNS - 1) {
-                    Cells[y][x].Cell.setFillColor(red);
+                    Cells[y][x].cell.setFillColor(red);
                 }
                 else if (y == 0 || y == ROWS - 1) {
-                    Cells[y][x].Cell.setFillColor(darkGreen);
+                    Cells[y][x].cell.setFillColor(darkGreen);
                 }
                 else {
                     if ((x + y) % 2 == 0) {
-                        Cells[y][x].Cell.setFillColor(lightGreen);
+                        Cells[y][x].cell.setFillColor(lightGreen);
                     }
                     else {
-                        Cells[y][x].Cell.setFillColor(leafGreen);
+                        Cells[y][x].cell.setFillColor(leafGreen);
                     }
                 }
-                window.draw(Cells[y][x].Cell);
+                window.draw(Cells[y][x].cell);
             }
         }
         while (window.pollEvent(event)) {
@@ -254,35 +186,33 @@ int main() {
                 window.close();
             }
             else if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
-                int x = event.mouseButton.x / blokWidth;
-                int y = event.mouseButton.y / blokHeight;
+                int x = event.mouseButton.x / blockWidth;
+                int y = event.mouseButton.y / blockHeight;
                 if (x >= 0 && x < ROWS && y >= 0 && y < COLUMNS) {
-                    Plant TempPlant(plantTexture , shooterTexture);
-                    TempPlant.setPosition(x , y);
+                    Plant TempPlant(plantTexture, bulletTexture);
+                    TempPlant.setPosition(x, y);
                     AllPlants.push_back(TempPlant);
-                    cout<<y<<x<<endl;
-                    // AllPlants.back().draw(window);
-                    // Spawn 
-                    shooterBalls.push_back(ShooterBall(shooterTexture, Cells[y][x].Cell.getPosition()));
+                    //cout << y << x << endl;
+                    bullets.push_back(Bullet(bulletTexture, Cells[y][x].cell.getPosition()));
                 }
             }
         }
         for (const auto& plant : AllPlants) {
-        plant.draw(window);
+            plant.draw(window);
         }
 
-        for (size_t i = 0; i < shooterBalls.size(); i++) {
-            shooterBalls[i].update(timeTaken);
-            shooterBalls[i].draw(window);
+        for (size_t i = 0; i < bullets.size(); i++) {
+            bullets[i].update(timeTaken);
+            bullets[i].draw(window);
         }
 
         zombie.update(timeTaken);
         zombie.draw(window);
 
         //hit-dishkiyaon
-        for (auto it = shooterBalls.begin(); it != shooterBalls.end();) {
+        for (auto it = bullets.begin(); it != bullets.end();) {
             if (zombie.getBounds().intersects(it->getBounds())) {
-                it = shooterBalls.erase(it);
+                it = bullets.erase(it);
                 zombie.takeDamage(1);
                 if (zombie.isDead()) {
                     // grid[0][0].increaseScore(50);
@@ -293,9 +223,8 @@ int main() {
                 ++it;
             }
         }
-        
-
         window.display();
     }
 
+    return 0;
 }
